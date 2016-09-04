0f86a5977b82f1a0c412dda26b3a8361714fd216
amirab24889 <amir_hossein39@yahoo.com>
2016-09-04 03:34:58 -0700

    Update index.php

--- index.php
+++ index.php
@@ -30,8 +30,8 @@ if (isset($update->edited_message)){
   $eid = $editm->message_id;
   $edname = $editm->from->first_name;
   $jsu = json_decode(file_get_contents(__DIR__.'/users/'.$eid.'.json'));
-  <code>یه بویی میاد🤔</code>\n<code>انگار باز یکی ادیت کرده😁</code>\n<code>الان مچشو میگیرم😂</code>\n<code>پیداش کردم عاغا</code>\n<b>".$edname."</b>\n"<code>ادیت کرده این متنو قبلا گفته بود</code>\n<code>".$jsu."</code>
-".$jsu;
+  $text = "<code>یه بویی میاد🤔</code>\n<code>انگار باز یکی ادیت کرده😁</code>\n<code>الان مچشو میگیرم😂</code>\n<code>پیداش کردم عاغا</code>\n<b>".$edname."</b>\n<code>ادیت کرده این متنو قبلا گفته بود</code>\n<code>".$jsu."</code>\n<code>گفتی</code>
+<code>".$jsu."</code>";
   $id = $update->edited_message->chat->id;
   bot('sendmessage',[
     'chat_id'=>$id,
